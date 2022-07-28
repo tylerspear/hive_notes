d0ac9fdded023069f70508c06c5953d9e34ad9ba
Tyler Spear <stylerspear@gmail.com>
2022-07-28 12:37:50 -0500

    added modify and delete routes, base CRUD functionality

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
+// ********* GLOBALS *********
 const express = require('express')
 const app = express()
 const PORT = 8500
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 require('dotenv').config()
 
+// ********* MIDDLEWARE *********
 //VIEW DIRECTORY AS ROOT
 app.set('views', path.join(__dirname, 'views'))
-
 //EJS AS TEMPLATE ENGINE
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
 
+
+app.use(express.static('public'))
+//OVERRIDE METHOD FOR PUT REQUESTS
+app.use(methodOverride('_method'))
 //setting extended as true allows us to pass an array
 app.use(express.urlencoded({extended: true}))
 
+// ********* DATABASE *********
 //HIVE SCHEMA
 const Hive = require('./models/hive')
-
 //DB connection
 mongoose.connect(process.env.DB_STRING,
     {useNewUrlParser: true},
     () => {console.log('Connected to HiveNotes database!')}
 )
 
+// ********* ROUTES *********
 //ALL HIVES
 app.get('/hives', async (req, res) => {
     const hives = await Hive.find({})
@@ -35,16 +41,38 @@ app.get('/hives/new', (req, res) => {
     res.render('newHive')
 })
 
-//ADD A NEW HIVE
+//ADD A NEW HIVE (NEW HIVE FORM ENDPOINT)
 app.post('/hives', async (req, res) => {
     const newHive = new Hive(req.body)
     await newHive.save()
     res.redirect('/hives')
 })
 
+app.get('/hives/:id', async (req, res) => {
+    const { id } = req.params
+    const hive = await Hive.findById(id)
+    res.render('showHive', { hive })
+})
+
 //SERVE FORM FOR HIVE EDIT
 app.get('/hives/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const hive = await Hive.findById(id)
+    res.render('editHive', { hive })
+})
 
+//MODIFY HIVE (ENDPOINT FOR editHive.ejs)
+app.put('/hives/:id', async (req, res) => {
+    const { id } = req.params
+    const hive = await Hive.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+    res.redirect(`/hives/${hive._id}`)
+})
+
+//DELETE ROUTE
+app.delete('/hives/:id', async (req, res) => {
+    const { id } = req.params
+    await Hive.findByIdAndDelete(id)
+    res.redirect('/hives')
 })
 
 //ACTIVATE PORT
